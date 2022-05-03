727366bf39bbcdfe9903fca3ce2a2dcb8a1a65c6
ray letoa <ray.letoa@gmail.com>
2022-05-03 15:20:02 +1200

    have taken out student F/L name from signup

--- components/missionComponents/introScene.js
+++ components/missionComponents/introScene.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import styles from '../../styles/mission/mission.module.css'
 import Image from 'next/image'
 import ButtonBar from '../../assets/missionAssets/buttonBar.png'
@@ -18,16 +18,42 @@ export default function IntroScene({setCurrentScene}) {
     const [canPlay, setCanPlay] = useState(false)
     const introScene = useRef()
     const router = useRouter()
-
+    
     const handleTimeChange = (e) => {
         setVideoTime(e.target.currentTime)
     }
+    
+    // const [video, setVideo] = useState()
+    // const [type, setType] = useState()
+    
+    // useEffect(() => {
+    //     const browser = navigator.userAgent
+    //     const chrome = browser.search(/Safari|Chrome/)
 
+    //     console.log(chrome)
+        
+    //     if(chrome > 90){
+    //         console.log('You are ON SAFARI')
+    //         setVideo("https://technius2022.s3.amazonaws.com/missionAssets/starsBackground.mp4")
+    //         setType("video/mp4")
+    //     } else {
+    //         console.log('You are NOT on SAFARI')
+    //         setVideo("https://technius-sydney.s3.ap-southeast-2.amazonaws.com/missionAssets/starsBackground.webm")
+    //         setType("video/webm")
+    //         // console.log(video)
+    //     }
+    // })
+    
+    // const mainVid = video
+    // const mainType = type
+    // console.log('we here point 1')
+    
     return (
         <>
             <video poster="/loading_screen.gif" playsInline ref={introScene} onTimeUpdate={handleTimeChange} className={styles.backgroundVideo} autoPlay muted>
-                {/* <source src={"https://technius2022.s3.amazonaws.com/missionAssets/starsBackground.mp4"} type="video/mp4" /> */}
                 <source src={"https://technius-sydney.s3.ap-southeast-2.amazonaws.com/missionAssets/starsBackground.webm"} type="video/webm" />
+                <source src={"https://technius2022.s3.amazonaws.com/missionAssets/starsBackground.mp4"} type="video/mp4" />
+                
                 video not supported in this browser
             </video>
             {videoTime > 0 && (
