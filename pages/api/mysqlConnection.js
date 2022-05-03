f4ed12782e36c89bb1b8c59caf9212b2e3050495
Stuart Toleafoa <stuarttoley@gmail.com>
2022-05-03 16:57:35 +1200

    Changed Database to Alans

--- pages/api/mysqlConnection.js
+++ pages/api/mysqlConnection.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2')
 
 const db = mysql.createPool({
-    host: "34.67.73.170",
-    user: "root",
-    password: "",
-    database: "technius",
+    host: "sg1-ts6.a2hosting.com",
+    user: "missio20_technius_admin",
+    password: "Mission14@#",
+    database: "missio20_technius",
     connectionLimit: 10,
 })
 
