e982691aa8a47f61377ae4e56e2214f26c95ffdc
Ray Letoa <ray.letoa@gmail.com>
2022-10-21 10:33:29 +1300

    section three layout done

--- pages/report/newReportCardSectionThree.js
+++ pages/report/newReportCardSectionThree.js
@@ -1,17 +1,43 @@
 import React from 'react'
 import Link from 'next/link'
 
-export default function newReportCardSectionThree() {
-  return (
-    <div>
-      newReportCardSectionThree
+import Style from '../../styles/reportPage/newReportCardSectionThree.module.css'
 
-      <br/>
-      <Link href='/report/newReportCard'>
-      <a>
-        LINK BACK TO MAIN COMPONENT
-      </a>
-    </Link>
+import Image from 'next/image'
+import ImageOne from '../../assets/missionAssets/newDevelopement/Group45.png'
+
+
+
+
+export default function newReportCardSectionThree() {
+
+
+
+  return (
+    <div className={Style.body}>
+      <div className={Style.bodyWrapper}>
+        <div className={Style.topTitle}>
+          What's <span className={Style.orange}>Next?</span>
+        </div>
+        <div className={Style.topContent}>
+        For your very first next step we reccomend choosing Math as a High School subject.<br/>
+        This gives you the widest range of choices in the tech field but it isn’t required for all<br/>
+        tech jobs. Visit ??? to find out more. 
+        </div>
+        <div className={Style.secondTitle}>
+          Advice from our <span className={Style.orange}>Tech leaders</span>
+        </div>
+        <div className={Style.secondContent}>
+          SECOND CONTENT BOX SCROLLABLE
+        </div>
+        <div>
+          <Link href='/report/newReportCard'>
+            <a>
+              BACK  
+            </a>
+          </Link>
+        </div>
+      </div>
     </div>
   )
 }
