2d2e9695dc2581bd15232a30e0e9d1003f940059
Ewan <zyc2339@gmail.com>
2022-10-25 23:35:54 +1300

    set up ewan's branch

---
+++ pages/whoAmISection.js
@@ -0,0 +1,10 @@
+import styles from "../styles/reportPage/new-reportPage.module.css";
+import Error404Image from "../assets/404Image.svg";
+
+export default function report() {
+  return (
+    <>
+      <div className={styles.reportPageRoot}>Ewan's page</div>
+    </>
+  );
+}
